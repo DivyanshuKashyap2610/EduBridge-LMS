a14ebdcd562117935679dd8ec62f9592e1cc41e3
DivyanshuKashyap2610 <divyanshkashyap261@gmail.com>
2025-09-07 18:11:38 +0530

    bugs

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -1,7 +1,11 @@
-import { SignIn } from '@clerk/nextjs'
+import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-  <div className='flex '><SignIn /></div>
-  )
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="w-full max-w-md">
+        <SignIn />
+      </div>
+    </div>
+  );
 }
